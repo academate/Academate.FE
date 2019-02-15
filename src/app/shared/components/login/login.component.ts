be1c3b18fb3a85d132869921d73e109e8e1a5d45
Salameh <hamed.salameh@philips.com>
2019-02-15 17:20:49 +0200

    support invalid credentials message

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -5,61 +5,67 @@ import { AuthService } from '../../services/auth.service';
 import { first } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  loading = false;
-  submitted = false;
-  returnUrl: string;
-  error = '';
+    loginForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
+    error = {};
 
-  constructor(
-      private formBuilder: FormBuilder,
-      private route: ActivatedRoute,
-      private router: Router,
-      private authenticationService: AuthService
-  ) { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthService
+    ) { }
 
-  ngOnInit() {
-      this.loginForm = this.formBuilder.group({
-          username: ['', Validators.required],
-          password: ['', Validators.required]
-      });
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
 
-      // reset login status
-      this.authenticationService.logout();
+        // reset login status
+        this.authenticationService.logout();
 
-      // get return url from route parameters or default to '/'
-      // tslint:disable-next-line:no-string-literal
-      this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-  }
+        // get return url from route parameters or default to '/'
+        // tslint:disable-next-line:no-string-literal
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
 
-  // convenience getter for easy access to form fields
-  get formControls() { return this.loginForm.controls; }
+    // convenience getter for easy access to form fields
+    get formControls() { return this.loginForm.controls; }
 
-  onSubmit() {
-      this.submitted = true;
-      // stop here if form is invalid
-      if (this.loginForm.invalid) {
-          return;
-      }
+    onSubmit() {
+        this.submitted = true;
+        // stop here if form is invalid
+        if (this.loginForm.invalid) {
+            return;
+        }
 
-      this.loading = true;
-      this.authenticationService.login(this.formControls.username.value, this.formControls.password.value)
-          .pipe(first())
-          .subscribe(
-              data => {
-                  this.router.navigateByUrl('/spa').then(
-                      () => this.router.navigateByUrl('/spa/(mc:acalendar)'),
-                      (err) => console.error(`Cannot navigate to home page ${JSON.stringify(err)}`)
-                  );
-              },
-              error => {
-                  this.error = error;
-                  this.loading = false;
-              });
-  }
+        this.loading = true;
+        this.authenticationService.login(this.formControls.username.value, this.formControls.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigateByUrl('/spa').then(
+                        () => this.router.navigateByUrl('/spa/(mc:acalendar)'),
+                        (err) => console.error(`Cannot navigate to home page ${JSON.stringify(err)}`)
+                    );
+                },
+                error => {
+                    if (error.originalError.originalError.error) {
+                        this.error = error.originalError.originalError.error;
+                    } else if (error.originalError.error) {
+                        this.error = error;
+                    } else {
+                        this.error = JSON.stringify(error);
+                    }
+                    this.loading = false;
+                });
+    }
 }
--- src/app/shared/interceptors/error-interceptor.service.ts
+++ src/app/shared/interceptors/error-interceptor.service.ts
@@ -1,36 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
-import { HttpStatusCode } from '../consts';
+import { HttpStatusCode, Consts } from '../consts';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ErrorInterceptor {
 
-  constructor(private authenticationService: AuthService) { }
+    networkErrors = [
+        0,
+        HttpStatusCode.BAD_REQUEST, // Originally not in the old client (happens when HMAC is invalid)
+        HttpStatusCode.NOT_FOUND,
+        HttpStatusCode.REQUEST_TIMEOUT,
+        HttpStatusCode.BAD_GATEWAY,
+        HttpStatusCode.SERVICE_UNAVAILABLE,
+        HttpStatusCode.GATEWAY_TIMEOUT
+    ];
+
+    constructor(private authenticationService: AuthService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
-            if (typeof err === 'string') {
-                return throwError(err);
-            } else if (err.status === HttpStatusCode.UNAUTHORIZED) {
-                // auto logout if 401 response returned from api
-                this.authenticationService.logout();
-                location.reload(true);
-            } else if (err.status === HttpStatusCode.BAD_REQUEST) {
-                // tslint:disable-next-line:no-string-literal
-                const httpError = err.error['error_description'];
-                return throwError(httpError);
-            } else if (err.status === 0) {
-                const httpError = 'Could not not contact the server';
-                return throwError(httpError);
-            }
-
-            const error = err.error.message || err.statusText;
-            return throwError(error);
-        }));
+        return next.handle(request).pipe(
+            catchError((err: HttpErrorResponse) => {
+                if (this.networkErrors.includes(err.status)) {
+                    return throwError({ message: 'Network Error', originalError: err, type: 'common', handled: true });
+                }
+                return throwError({originalError: err, handled: false});
+            }));
     }
 }
